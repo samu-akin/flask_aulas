081b3360297b39f35ab69e60a311243647d9129e
samu-akin <129891579+samu-akin@users.noreply.github.com>
2024-10-16 17:56:02 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,10 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, make_response, redirect, abort;
+app = Flask(__name__);
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';
+    html_texto += '<ul>';
+    html_texto += '<li><a href="/">Home</a>';
+    html_texto += '<li><a href="/user/Fabio Teixeira/PT23820X/IFSP">Identificação</a>';
+    html_texto += '<li><a href="/contextorequisicao">Contexto da requisição</a>';
+    html_texto += '</ul>';
+
+    return html_texto;
+
+@app.route('/user/<name>/<prontuario>/<institution>')
+def user(name, prontuario, institution):
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';
+    html_texto += '<h2>Aluno: {}</h2>'.format(name);
+    html_texto += '<h2>Prontuário: {}</h2>'.format(prontuario);
+    html_texto += '<h2>Instituição: {}</h2>'.format(institution);
+    html_texto += '<p><a href="/">Voltar</a></p>';
+
+    return html_texto;
+
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent');
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';
+    html_texto += '<h2>Seu navegador é: {}</h2>'.format(user_agent);
+    html_texto += '<h2>O IP do cumputador remoto é: {}</h2>'.format(request.remote_addr);
+    html_texto += '<h2>O host da aplicação é: {}</h2>'.format(request.host);
+    html_texto += '<p><a href="/">Voltar</a></p>';
+
+    return html_texto;
